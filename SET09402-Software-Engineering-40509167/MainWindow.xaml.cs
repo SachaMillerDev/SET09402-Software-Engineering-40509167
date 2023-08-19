2c0aea34480a20c3cda771a66d98079b87b7995d
SachaMillerDev <sachamiller.hi@gmail.com>
2023-08-19 10:02:19 +0100

    update to the UI

--- SET09402-Software-Engineering-40509167/MainWindow.xaml.cs
+++ SET09402-Software-Engineering-40509167/MainWindow.xaml.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 using Microsoft.Win32;
 
 namespace SET09402_Software_Engineering_40509167
@@ -22,35 +20,46 @@ namespace SET09402_Software_Engineering_40509167
         {
             string messageType = "";
             string messageContent = "";
+
             if (smsRadioButton.IsChecked == true)
             {
                 messageType = "SMS:";
-                messageContent = messageType + messageInput.Text;
+                messageContent = messageType + " Phone Number: " + smsPhoneNumberInput.Text + "; Message: " + messageInput.Text;
             }
             else if (emailRadioButton.IsChecked == true)
             {
                 messageType = "Email:";
-                messageContent = messageType + "Recipient: " + emailRecipientInput.Text + "; Subject: " + emailSubjectInput.Text + "; Body: " + messageInput.Text;
+                messageContent = messageType + " Recipient: " + emailRecipientInput.Text + "; Subject: " + emailSubjectInput.Text + "; Body: " + messageInput.Text;
+                if (incidentCheckBox.IsChecked == true)
+                {
+                    messageContent += "; Incident Type: " + ((ComboBoxItem)incidentComboBox.SelectedItem).Content;
+                }
             }
             else if (tweetRadioButton.IsChecked == true)
             {
                 messageType = "Tweet:";
-                messageContent = messageType + messageInput.Text;
+                messageContent = messageType + " Username: " + tweetUsernameInput.Text + "; Message: " + messageInput.Text;
             }
+
             File.AppendAllText("TestFile.txt", messageContent + Environment.NewLine);
+
             MessageProcessor processor = new MessageProcessor();
             MessageReader reader = new MessageReader("TestFile.txt");
             JSONOutput jsonOutput = new JSONOutput { OutputFile = "Output.json" };
+
             Message message = reader.ReadMessage();
             while (message != null)
             {
                 processor.AddMessage(message);
                 message = reader.ReadMessage();
             }
+
             jsonOutput.WriteToJSON(processor.Messages);
+
             string jsonContent = File.ReadAllText("Output.json");
             string newOutput = "New Message: " + jsonContent;
             outputTextBlock.Text = newOutput + "\n\n" + outputTextBlock.Text;
+
             DisplayLists();
         }
 
@@ -70,11 +79,13 @@ namespace SET09402_Software_Engineering_40509167
             {
                 TrendingList.Items.Add(item.Key + ": " + item.Value);
             }
+
             MentionsList.Items.Clear();
             foreach (var item in TweetMessage.MentionsDictionary.OrderByDescending(x => x.Value))
             {
                 MentionsList.Items.Add(item.Key + ": " + item.Value);
             }
+
             SIRList.Items.Clear();
             foreach (var item in EmailMessage.SIRList)
             {
@@ -84,35 +95,46 @@ namespace SET09402_Software_Engineering_40509167
 
         private void SmsRadioButton_Checked(object sender, RoutedEventArgs e)
         {
+            smsPhoneNumberInput.Visibility = Visibility.Visible;
+            tweetUsernameInput.Visibility = Visibility.Collapsed;
             emailRecipientInput.Visibility = Visibility.Collapsed;
             emailSubjectInput.Visibility = Visibility.Collapsed;
+            incidentCheckBox.Visibility = Visibility.Collapsed;
+            incidentComboBox.Visibility = Visibility.Collapsed;
             messageInput.Text = "Text here";
-            messageInput.Foreground = Brushes.Gray;
+            messageInput.Foreground = System.Windows.Media.Brushes.Gray;
         }
 
         private void EmailRadioButton_Checked(object sender, RoutedEventArgs e)
         {
+            smsPhoneNumberInput.Visibility = Visibility.Collapsed;
+            tweetUsernameInput.Visibility = Visibility.Collapsed;
             emailRecipientInput.Visibility = Visibility.Visible;
             emailSubjectInput.Visibility = Visibility.Visible;
+            incidentCheckBox.Visibility = Visibility.Visible;
             messageInput.Text = "Body here";
-            messageInput.Foreground = Brushes.Gray;
+            messageInput.Foreground = System.Windows.Media.Brushes.Gray;
         }
 
         private void TweetRadioButton_Checked(object sender, RoutedEventArgs e)
         {
+            smsPhoneNumberInput.Visibility = Visibility.Collapsed;
+            tweetUsernameInput.Visibility = Visibility.Visible;
             emailRecipientInput.Visibility = Visibility.Collapsed;
             emailSubjectInput.Visibility = Visibility.Collapsed;
+            incidentCheckBox.Visibility = Visibility.Collapsed;
+            incidentComboBox.Visibility = Visibility.Collapsed;
             messageInput.Text = "Text here";
-            messageInput.Foreground = Brushes.Gray;
+            messageInput.Foreground = System.Windows.Media.Brushes.Gray;
         }
 
         private void TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
             TextBox tb = sender as TextBox;
-            if (tb.Text == "Text here" || tb.Text == "Recipient" || tb.Text == "Subject" || tb.Text == "Body here")
+            if (tb.Text == "Text here" || tb.Text == "Recipient" || tb.Text == "Subject" || tb.Text == "Body here" || tb.Text == "Phone Number" || tb.Text == "Username")
             {
                 tb.Text = "";
-                tb.Foreground = Brushes.Black;
+                tb.Foreground = System.Windows.Media.Brushes.Black;
             }
         }
 
@@ -121,15 +143,13 @@ namespace SET09402_Software_Engineering_40509167
             TextBox tb = sender as TextBox;
             if (string.IsNullOrWhiteSpace(tb.Text))
             {
-                if (tb == messageInput && emailRadioButton.IsChecked == true)
-                    tb.Text = "Body here";
-                else if (tb == messageInput)
-                    tb.Text = "Text here";
-                else if (tb == emailRecipientInput)
-                    tb.Text = "Recipient";
-                else if (tb == emailSubjectInput)
-                    tb.Text = "Subject";
-                tb.Foreground = Brushes.Gray;
+                if (tb == messageInput && emailRadioButton.IsChecked == true) tb.Text = "Body here";
+                else if (tb == messageInput) tb.Text = "Text here";
+                else if (tb == emailRecipientInput) tb.Text = "Recipient";
+                else if (tb == emailSubjectInput) tb.Text = "Subject";
+                else if (tb == smsPhoneNumberInput) tb.Text = "Phone Number";
+                else if (tb == tweetUsernameInput) tb.Text = "Username";
+                tb.Foreground = System.Windows.Media.Brushes.Gray;
             }
         }
 
